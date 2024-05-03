3498be45e3924daf1889771769325b8ab5e9d536
Devarshi Trivedi <84924496+Devarshii@users.noreply.github.com>
2024-05-03 10:31:27 +0530

    Add files via upload

---
+++ includes/sidebarmenu.php
@@ -0,0 +1,61 @@
+<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   <li id="menu-academico-avaliacoes" ><a href="create-package.php">Create</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">Manage</a></li>
+										  </ul>
+										</li>
+									<li id="menu-academico">
+										<a href="#">
+											<i class="fa fa-list-ul" aria-hidden="true"></i>
+											<span> Equipments</span>
+											<span class="fa fa-angle-right" style="float: right"></span>
+											<div class="clearfix"></div>
+										</a>
+										<ul id="menu-academico-sub" >
+									   		<li id="menu-academico-avaliacoes" ><a href="create-equipment.php">Create</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-equipments.php">Manage</a></li>
+									  	</ul>
+									</li>
+									<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+									<li id="menu-academico">
+										<a href="#">
+											<i class="fa fa-list-ul" aria-hidden="true"></i>
+											<span>Bookings</span>
+											<span class="fa fa-angle-right" style="float: right"></span>
+											<div class="clearfix"></div>
+										</a>
+										<ul id="menu-academico-sub" >
+											<li><a href="manage-bookings.php"><span>Tour</span></a></li>
+											<li><a href="manage-activity-bookings.php"><span>Activity</span></a></li>
+											<li><a href="manage-equipment-bookings.php"><span>Equipment</span></a></li>
+									  	</ul>
+									</li>
+									 <li><a href="manageissues.php"><i class="fa fa-table"></i>  <span>Manage Issues</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Enquiries</span><div class="clearfix"></div></a></li>
+									<li id="menu-academico">
+										<a href="#">
+											<i class="fa fa-list-ul" aria-hidden="true"></i>
+											<span>Adventure Activities</span>
+											<span class="fa fa-angle-right" style="float: right"></span>
+											<div class="clearfix"></div>
+										</a>
+										<ul id="menu-academico-sub" >
+									   		<li id="menu-academico-avaliacoes" ><a href="create-activities.php">Create</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-activities.php">Manage</a></li>
+									  	</ul>
+									</li>
+									<li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Pages</span><div class="clearfix"></div></a></li>
+							     
+									
+								  </ul>
+								</div>
+							  </div>
